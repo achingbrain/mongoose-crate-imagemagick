730133ad91cbb7098d679836b39cc224bd1b5183
achingbrain <alex@achingbrain.net>
2014-07-14 10:34:58 +0100

    bumped deps and reformat

--- README.md
+++ README.md
@@ -13,55 +13,55 @@ A modern version of ImageMagick installed and available on the path.  You may al
 You can pass images through one or more ImageMagick filters:
 
 ```javascript
-var mongoose = require("mongoose"),
-  crate = require("mongoose-crate"),
-  LocalFS = require("mongoose-crate-localfs"),
-  ImageMagick = require("mongoose-crate-imagemagick");
+var mongoose = require('mongoose'),
+  crate = require('mongoose-crate'),
+  LocalFS = require('mongoose-crate-localfs'),
+  ImageMagick = require('mongoose-crate-imagemagick')
 
 var PostSchema = new mongoose.Schema({
   title: String,
   description: String
-});
+})
 
 PostSchema.plugin(crate, {
   storage: new LocalFS({
-    directory: "/path/to/storage/directory"
+    directory: '/path/to/storage/directory'
   }),
   fields: {
     image: {
       processor: new ImageMagick({
-        tmpDir: "/tmp", // Where transformed files are placed before storage, defaults to os.tmpdir()
-        formats: ["JPEG", "GIF", "PNG"], // Supported formats, defaults to ["JPEG", "GIF", "PNG", "TIFF"]
+        tmpDir: '/tmp', // Where transformed files are placed before storage, defaults to os.tmpdir()
+        formats: ['JPEG', 'GIF', 'PNG'], // Supported formats, defaults to ['JPEG', 'GIF', 'PNG', 'TIFF']
         transforms: {
           original: {
             // keep the original file
           },
           small: {
-            resize: "150x150",
-            format: ".jpg"
+            resize: '150x150',
+            format: '.jpg'
           },
           medium: {
-            resize: "250x250",
-            format: ".jpg"
+            resize: '250x250',
+            format: '.jpg'
           }
         }
       })
     }
   }
-});
+})
 
-var Post = mongoose.model("Post", PostSchema);
+var Post = mongoose.model('Post', PostSchema)
 ```
 
 .. then later:
 
 ```javascript
-var post = new Post();
-post.attach("image", {path: "/path/to/image"}, function(error) {
-	// image is now attached and post.image is populated e.g.:
-	// post.image.small.url
-	// post.image.medium.url
-});
+var post = new Post()
+post.attach('image', {path: '/path/to/image'}, function(error) {
+  // image is now attached and post.image is populated e.g.:
+  // post.image.small.url
+  // post.image.medium.url
+})
 ```
 
 ## Meta data
@@ -73,10 +73,10 @@ post.attach("image", {path: "/path/to/image"}, function(error) {
   width: 120,
   height: 103,
   depth: 8,
-  format: "PNG",
-  name: "dragon.png",
+  format: 'PNG',
+  name: 'dragon.png',
   size: 26887,
-  url: "http://my_bucket.s3.amazonaws.com/folder/4fbaaa31db8cec0923000019-medium.png"
+  url: 'http://my_bucket.s3.amazonaws.com/folder/4fbaaa31db8cec0923000019-medium.png'
 }
 ```
 
@@ -98,8 +98,8 @@ PostSchema.plugin(crate, {
       processor: new ImageMagick({
         transforms: {
           small: {
-            crop: "120x120",
-            blur: "5x10"
+            crop: '120x120',
+            blur: '5x10'
 ```
 
 For more information on available transforms, see the [ImageMagick website](http://www.imagemagick.org/script/command-line-options.php).
@@ -116,7 +116,7 @@ PostSchema.plugin(crate, {
       processor: new ImageMagick({
         transforms: {
           as_jpeg: {
-            format: "jpg"
+            format: 'jpg'
 ```
 
 ### Supported formats
@@ -131,7 +131,7 @@ PostSchema.plugin(crate, {
   fields: {
     image: {
       processor: new ImageMagick({
-        formats: ["JPEG", "GIF", "PNG"]
+        formats: ['JPEG', 'GIF', 'PNG']
 ```
 
 The values should match up with `convert`'s supported formats.  To see a list of all formats supported by your ImageMagick install, run:
--- test/ImageMagickTest.js
+++ test/ImageMagickTest.js
@@ -1,132 +1,132 @@
-var should = require("should"),
-	path = require("path"),
-	sinon = require("sinon"),
-	ImageMagick = require("../lib/ImageMagick");
+var should = require('should'),
+  path = require('path'),
+  sinon = require('sinon'),
+  ImageMagick = require('../lib/ImageMagick')
 
-describe("ImageMagick", function() {
+describe('ImageMagick', function() {
 
-	it("should object strenuously if options are not specified", function(done) {
-		(function() {
-			new ImageMagick();
-		}).should.throw();
+  it('should object strenuously if options are not specified', function(done) {
+    (function() {
+      new ImageMagick()
+    }).should.throw()
 
-		done();
-	})
+    done()
+  })
 
-	it("should process a png", function(done) {
-		var processor = new ImageMagick({
-			transforms: {
-				original: {
+  it('should process a png', function(done) {
+    var processor = new ImageMagick({
+      transforms: {
+        original: {
 
-				}
-			}
-		});
+        }
+      }
+    })
 
-		var file = path.resolve(__dirname + "/./fixtures/node_js_logo.png");
-		var storageProvider = {
-			save: sinon.stub()
-		};
-		storageProvider.save.callsArgWith(1, null, "http://foo.bar");
+    var file = path.resolve(__dirname + '/./fixtures/node_js_logo.png')
+    var storageProvider = {
+      save: sinon.stub()
+    }
+    storageProvider.save.callsArgWith(1, null, 'http://foo.bar')
 
-		var model = {
-			id: "model_id",
-			original: {}
-		};
+    var model = {
+      id: 'model_id',
+      original: {}
+    }
 
-		processor.process({
-			path: file,
-			mimeType: "image/png",
-			name: "node_js_logo.png"
-		}, storageProvider, model, function(error) {
-			should(error).not.ok;
+    processor.process({
+      path: file,
+      mimeType: 'image/png',
+      name: 'node_js_logo.png'
+    }, storageProvider, model, function(error) {
+      should(error).not.ok
 
-			// should have populated meta data
-			model.original.should.be.ok;
-			model.original.depth.should.equal(8);
-			model.original.format.should.equal("PNG");
-			model.original.height.should.equal(196);
-			model.original.width.should.equal(574);
-			model.original.type.should.equal("image/png");
-			model.original.name.should.equal("node_js_logo.png");
-			model.original.url.should.equal("http://foo.bar");
+      // should have populated meta data
+      model.original.should.be.ok
+      model.original.depth.should.equal(8)
+      model.original.format.should.equal('PNG')
+      model.original.height.should.equal(196)
+      model.original.width.should.equal(574)
+      model.original.type.should.equal('image/png')
+      model.original.name.should.equal('node_js_logo.png')
+      model.original.url.should.equal('http://foo.bar')
 
-			// this can vary depending on file system...
-			model.original.size.should.be.greaterThan(19000);
+      // this can vary depending on file system...
+      model.original.size.should.be.greaterThan(19000)
 
-			done();
-		});
-	})
+      done()
+    })
+  })
 
-	it("should not process text file", function(done) {
-		var processor = new ImageMagick({
-			transforms: {
-				original: {
+  it('should not process text file', function(done) {
+    var processor = new ImageMagick({
+      transforms: {
+        original: {
 
-				}
-			}
-		});
-		var file = path.resolve(__dirname + "/./fixtures/foo.txt");
+        }
+      }
+    })
+    var file = path.resolve(__dirname + '/./fixtures/foo.txt')
 
-		processor.process(file, null, null, function(error) {
-			should(error).ok;
+    processor.process(file, null, null, function(error) {
+      should(error).ok
 
-			done();
-		});
-	})
+      done()
+    })
+  })
 
-	it("should extend field schema", function(done) {
-		var processor = new ImageMagick({
-			transforms: {
-				original: {
+  it('should extend field schema', function(done) {
+    var processor = new ImageMagick({
+      transforms: {
+        original: {
 
-				}
-			}
-		});
+        }
+      }
+    })
 
-		var schema = processor.createFieldSchema();
-		schema.original.format.should.equal(String);
-		schema.original.depth.should.equal(Number);
-		schema.original.width.should.equal(Number);
-		schema.original.height.should.equal(Number);
+    var schema = processor.createFieldSchema()
+    schema.original.format.should.equal(String)
+    schema.original.depth.should.equal(Number)
+    schema.original.width.should.equal(Number)
+    schema.original.height.should.equal(Number)
 
-		done();
-	})
+    done()
+  })
 
-	it("should transform a png", function(done) {
-		var processor = new ImageMagick({
-			transforms: {
-				smaller: {
-					"resize": "100x34",
-					"format": "jpg"
-				}
-			}
-		});
+  it('should transform a png', function(done) {
+    var processor = new ImageMagick({
+      transforms: {
+        smaller: {
+          'resize': '100x34',
+          'format': 'jpg'
+        }
+      }
+    })
 
-		var file = path.resolve(__dirname + "/./fixtures/node_js_logo.png");
-		var storageProvider = {
-			save: function(path, callback) {
-				callback(null, path)
-			}
-		};
+    var file = path.resolve(__dirname + '/./fixtures/node_js_logo.png')
+    var storageProvider = {
+      save: function(path, callback) {
+        callback(null, path)
+      }
+    }
 
-		var model = {
-			id: "model_id",
-			smaller: {}
-		};
+    var model = {
+      id: 'model_id',
+      smaller: {}
+    }
 
-		processor.process({
-			path: file,
-			mimeType: "image/png",
-			name: "node_js_logo.png"
-		}, storageProvider, model, function(error) {
-			should(error).not.ok;
+    processor.process({
+      path: file,
+      mimeType: 'image/png',
+      name: 'node_js_logo.png'
+    }, storageProvider, model, function(error) {
+      should(error).not.ok
 
-			model.smaller.format.should.equal("JPEG");
-			model.smaller.width.should.equal(100);
-			model.smaller.height.should.equal(34);
-			model.smaller.type.should.equal("image/jpeg");
+      model.smaller.format.should.equal('JPEG')
+      model.smaller.width.should.equal(100)
+      model.smaller.height.should.equal(34)
+      model.smaller.type.should.equal('image/jpeg')
 
-			done();
-		});
-	})
+      done()
+    })
+  })
 })
